2f60f27e49ac9b755d31c88c72876cba746e7b74
masineediaditya <aditya@arokee.com>
2018-03-09 09:54:04 +0530

    eeee

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,9 @@
 
 import { Component } from '@angular/core';
 import { NavController,NavParams } from 'ionic-angular';
-import {GooglePlus} from '@ionic-native/google-plus';
-import {AngularFireModule} from 'angularfire2';
-import firebase from 'firebase';
-import { LoginPage } from '../login/login';
+// import {GooglePlus} from '@ionic-native/google-plus';
+
+
 
 
 @Component({
@@ -13,21 +12,21 @@ import { LoginPage } from '../login/login';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public googleplus:GooglePlus,public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
   }
-  login(){
-    this.googleplus.login({
-      'webClientId':'430745713210-tou81pvel86ou4jad442o4kmukttepes.apps.googleusercontent.com',
-      'offline':true
-    }).then(res=>{
-      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
-      .then(suc=>{
-       this.navCtrl.push(LoginPage);
-      }).catch(ns=>{
-        alert('not succes');
-      })
-    })
-  }
+  // login(){
+  //   this.googleplus.login({
+  //     'webClientId':'430745713210-tou81pvel86ou4jad442o4kmukttepes.apps.googleusercontent.com',
+  //     'offline':true
+  //   }).then(res=>{
+  //     firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+  //     .then(suc=>{
+  //      this.navCtrl.push(LoginPage);
+  //     }).catch(ns=>{
+  //       alert('not succes');
+  //     })
+  //   })
+  // }
 
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,9 @@
+import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {GooglePlus} from '@ionic-native/google-plus';
+import {AngularFireModule} from 'angularfire2';
+import firebase from 'firebase';
 /**
  * Generated class for the LoginPage page.
  *
@@ -15,11 +18,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public googleplus:GooglePlus) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  login(){
+    this.googleplus.login({
+      'webClientId':'430745713210-tou81pvel86ou4jad442o4kmukttepes.apps.googleusercontent.com',
+      'offline':true
+    }).then(res=>{
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+      .then(suc=>{
+       this.navCtrl.push(HomePage);
+      }).catch(ns=>{
+        alert('not succes');
+      })
+    })
   }
 
 }
